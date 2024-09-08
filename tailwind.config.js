c3166f4574322f65efe1f34aacc854cedbcf2645
MaximeFontenay <maximefontenay1@gmail.com>
2024-09-08 10:41:39 +0200

    Base layout and setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        xs: '14px',
+        sm: '16px',
+        base: '18px',
+        lg: '24px',
+        xl: '32px',
+        '2xl': '36px',
+        '3xl': '80px',
+        '4xl': '96px',
+      },
+      colors: {
+        primary: '#0089DC',
+        secondary: '#F5D70E',
+        tertiary: '#5D5AFE',
+      },
+      fontFamily: {
+        satoshi: ['Satoshi', 'Poppins', 'sans-serif'],
+        poppins: ['Poppins', 'Satoshi', 'sans-serif'],
+      },
+    },
     container: {
       center: true,
     },
