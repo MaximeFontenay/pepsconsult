c2acf7a184c94cae95ac6ac2590b1c039f354ad4
MaximeFontenay <maximefontenay1@gmail.com>
2024-09-22 12:22:26 +0200

    [ADD] Pages services + assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,6 +29,23 @@ module.exports = {
       center: true,
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addBase, theme }) {
+      function extractColorVars(colorObj, colorGroup = '') {
+        return Object.keys(colorObj).reduce((vars, colorKey) => {
+          const value = colorObj[colorKey]
+          const cssVariable = colorKey === 'DEFAULT' ? `--color${colorGroup}` : `--color${colorGroup}-${colorKey}`
+
+          const newVars = typeof value === 'string' ? { [cssVariable]: value } : extractColorVars(value, `-${colorKey}`)
+
+          return { ...vars, ...newVars }
+        }, {})
+      }
+
+      addBase({
+        ':root': extractColorVars(theme('colors')),
+      })
+    },
+  ],
   darkMode: ['class'],
 }
