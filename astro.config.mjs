95660aa1c3183b2545a071cf91bfe9c0efbaaf05
Maxf <maximefontenay1@gmail.com>
2024-10-14 10:44:13 +0200

    [ADD] Mentions légales + Lottie homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,25 @@ import tailwind from '@astrojs/tailwind'
 import compress from 'astro-compress'
 import { defineConfig } from 'astro/config'
 
-import vue from '@astrojs/vue';
+import vue from '@astrojs/vue'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.pepsconsult.fr',
   output: 'static',
   compressHTML: true,
-  integrations: [tailwind({
-    applyBaseStyles: false,
-  }), compress(), vue()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    compress(),
+    vue(),
+  ],
   prefetch: {
     prefetchAll: true,
     defaultStrategy: 'viewport',
+  },
+  assetsInclude: {
+    'src/assets': true,
   },
 })
---
+++ src/globals.ts
@@ -0,0 +1,3 @@
+export const phone = '07 49 37 62 21'
+export const email = 'nada.bitar@pepsconsult.fr'
+export const address = '5 rue de la République, 69001 Lyon'
