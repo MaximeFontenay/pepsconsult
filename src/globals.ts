e789606f44a3cd07409022b57b42b96bc71a7714
MaximeFontenay <maximefontenay1@gmail.com>
2024-11-29 11:27:39 +0100

    [CHANGE] Modif whatsapp number in footer btn

--- src/globals.ts
+++ src/globals.ts
@@ -1,3 +1,4 @@
 export const phone = '07 49 37 62 21'
+export const whatsapp = '+33749376221'
 export const email = 'nada.bitar@pepsconsult.fr'
 export const address = '5 rue de la République, 69001 Lyon'
